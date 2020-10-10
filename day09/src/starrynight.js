ce68ba802b2a3939fd7ea5aa9c4d37dd35a25c85
Aakash Adesara <aakash.adesara@gmail.com>
2020-10-09 17:36:03 -0700

    removed unused code

--- day09/src/starrynight.js
+++ day09/src/starrynight.js
@@ -1,6 +1,11 @@
+// init
 const Init = (container) => {
     var canvas = createCanvas(container)
     initMovingStarryNight(canvas)
+}
+
+const starrynight = {
+    init: Init
 }
 
 const createCanvas = (container) => {
@@ -11,22 +16,7 @@ const createCanvas = (container) => {
     return canvas;
 }
 
-class Arc {
-    constructor(width, radius, color, start, end, velocity) {
-        this.width      = width
-        this.radius     = radius
-        this.color      = color
-        this.start      = start
-        this.end        = end
-        this.velocity   = velocity
-    }
-
-    step() {
-        this.start = this.start + this.velocity
-        this.end = this.end + this.velocity
-    }
-}
-
+// paint
 const generateArcs = (c) => {
     const SATURATION = 50
     const iters = Math.max(c.width, c.height)
@@ -37,7 +27,7 @@ const generateArcs = (c) => {
         const START = (Math.random() * 2 * Math.PI)
         const END = START + (Math.random() * Math.PI / 2)
         const WIDTH = Math.floor(Math.random() * 3) + 1
-        const VELOCITY = 0.00 + Math.random() / 100
+        const VELOCITY = (0.00 + Math.random() / 100)/4
         const COLORS = ['#ffefed', '#fad9d4', '#fae1d4', '#fcf2ed', '#fcf8ed', '#fcf2d7', '#fcc7c2']
 
         if (randomEnabled(SATURATION)) {
@@ -53,38 +43,6 @@ const generateArcs = (c) => {
     }
 
     return arcs
-}
-
-
-const paintBackground = (c, topLeftColor, topRightColor, bottomLeftColor, bottomRightColor) => {
-    var ctx = c.getContext("2d");
-    
-    const width = c.width;
-    const height = c.height;
-
-    const BLOCK_SIZE = 10
-    for(var x = 0; x < width; x+=BLOCK_SIZE) {
-        for(var y = 0; y < height; y+=BLOCK_SIZE) {
-            ctx.beginPath();
-
-            const div = Math.max(width, height)
-
-            const p_a = (div - Math.sqrt((x) ** 2 + (y) ** 2)) / div
-            const p_b = (div - Math.sqrt((x - width) ** 2 + (y) ** 2)) / div
-            const p_c = (div - Math.sqrt((x) ** 2 + (y - height) ** 2 )) / div
-            const p_d = (div - Math.sqrt((x - width) ** 2, (y - height) ** 2)) / div
-
-            var r = (p_a * topLeftColor.r + p_b * topRightColor.r + p_c * bottomLeftColor.r + p_d * bottomRightColor.r )
-            var g = (p_a * topLeftColor.g + p_b * topRightColor.g + p_c * bottomLeftColor.g + p_d * bottomRightColor.g )
-            var b = (p_a * topLeftColor.b + p_b * topRightColor.b + p_c * bottomLeftColor.b + p_d * bottomRightColor.b )
-
-            ctx.strokeStyle = `rgba(${r}, ${g}, ${b})`
-            ctx.fillStyle = `rgba(${r}, ${g}, ${b})`
-            ctx.rect(x, y, BLOCK_SIZE, BLOCK_SIZE);
-            ctx.fill()
-            ctx.stroke();
-        }
-    }
 }
 
 const paintStars = (c, arcs) => {
@@ -111,9 +69,7 @@ const initStarryNight = (c) => {
 }
 
 const initMovingStarryNight = (c) => {
-
     const arcs = generateArcs(c)
-
     setInterval(
         () => {
             
@@ -140,6 +96,7 @@ const eraseCanvas = (c) => {
     ctx.stroke();
 }
 
+// util
 const randColorValue = () => {
     return Math.floor(Math.random() * 80)
 }
@@ -148,7 +105,19 @@ const randomEnabled = (prob) => {
     return Math.floor(Math.random() * 100) < prob
 }
 
-const starrynight = {
-    init: Init
-}
+// classes
+class Arc {
+    constructor(width, radius, color, start, end, velocity) {
+        this.width      = width
+        this.radius     = radius
+        this.color      = color
+        this.start      = start
+        this.end        = end
+        this.velocity   = velocity
+    }
 
+    step() {
+        this.start = this.start + this.velocity
+        this.end = this.end + this.velocity
+    }
+}
